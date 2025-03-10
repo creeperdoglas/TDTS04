6681423e51cceb1334d64ad075fc182a771da3d4
creeperdoglas <borenmelkan@gmail.com>
2025-03-10 13:41:03 +0100

    one step closer, maybe finished

--- labb 4/RouterNode.py
+++ labb 4/RouterNode.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-#code by melgu374 and antfo614
+# code by melgu374 and antfo614
 import GuiTextArea, RouterPacket, F
 from copy import deepcopy
+from F import F
 
 class RouterNode():
     def __init__(self, ID, sim, costs):
@@ -12,7 +13,6 @@ class RouterNode():
         self.costs = deepcopy(costs)
         self.neighbors = [i for i in range(len(costs)) if costs[i] != sim.INFINITY and i != ID]
         self.distanceTable = [[sim.INFINITY]*sim.NUM_NODES for _ in range(sim.NUM_NODES)]
-        self.distanceVectors = [[sim.INFINITY for _ in range(sim.NUM_NODES)] for _ in range(sim.NUM_NODES)]
         self.distanceVector = deepcopy(costs)
         self.nextHops = [i if costs[i] != sim.INFINITY else None for i in range(sim.NUM_NODES)]
 
@@ -21,27 +21,32 @@ class RouterNode():
 
     def initRouteTable(self):
         for i in range(self.sim.NUM_NODES):
+            self.distanceTable[self.myID][i] = self.costs[i]
             if self.costs[i] != self.sim.INFINITY and i != self.myID:
-                self.distanceTable[self.myID][i] = self.costs[i]
                 self.nextHops[i] = i
             else:
-                self.distanceTable[self.myID][i] = self.sim.INFINITY
                 self.nextHops[i] = None
 
     def recvUpdate(self, pkt):
         source = pkt.sourceid
-        self.distanceVectors[source] = pkt.mincost
         updated = False
 
+        # Spara mottagen distansvektor från granne
+        for dest in range(self.sim.NUM_NODES):
+            self.distanceTable[source][dest] = pkt.mincost[dest]
+
+        # Uppdatera vår egen distansvektor baserat på nya uppgifter
         for dest in range(self.sim.NUM_NODES):
             if dest == self.myID:
                 continue
+
             min_cost = self.costs[dest]
             next_hop = self.nextHops[dest]
 
             for neighbor in self.neighbors:
-                if self.costs[neighbor] + self.distanceVectors[neighbor][dest] < min_cost:
-                    min_cost = self.costs[neighbor] + self.distanceVectors[neighbor][dest]
+                potential_cost = self.costs[neighbor] + self.distanceTable[neighbor][dest]
+                if potential_cost < min_cost:
+                    min_cost = potential_cost
                     next_hop = neighbor
 
             if min_cost != self.distanceVector[dest]:
@@ -65,26 +70,48 @@ class RouterNode():
             self.sim.toLayer2(packet)
 
     def updateLinkCost(self, dest, newcost):
-        self.costs[dest] = newcost = newcost
-        self.distanceVector[dest] = newcost
+        self.costs[dest] = newcost
         self.distanceTable[self.myID][dest] = newcost
 
         updated = False
 
-        for dest in range(self.sim.NUM_NODES):
-            min_cost = min(self.costs[neighbor] + self.distanceVectors[neighbor][dest] for neighbor in self.neighbors)
-            if min_cost != self.distanceVector[dest]:
-                self.distanceVector[dest] = min_cost
+        for dst in range(self.sim.NUM_NODES):
+            if dst == self.myID:
+                continue
+
+            min_cost = min(
+                self.costs[neighbor] + self.distanceTable[neighbor][dst]
+                for neighbor in self.neighbors
+            )
+
+            if min_cost != self.distanceVector[dst]:
+                self.distanceVector[dst] = min_cost
+                self.nextHops[dst] = min(
+                    self.neighbors,
+                    key=lambda neighbor: self.costs[neighbor] + self.distanceTable[neighbor][dst]
+                )
                 updated = True
 
         if updated:
             self.propagate()
 
     def printDistanceTable(self):
-        self.myGUI.println(f"Distance table for router #{self.myID} at time {self.sim.getClocktime()}")
-        self.myGUI.println("Dest | Cost | Next hop")
-        self.myGUI.println("------------------------")
-        for dest in range(self.sim.NUM_NODES):
-            if dest != self.myID:
-                next_hop = self.nextHops[dest] if self.nextHops[dest] is not None else '-'
-                self.myGUI.println(f"{dest}    | {self.distanceVector[dest]}    | {next_hop}")
+        self.myGUI.println(f"Current state for router {self.myID} at time {self.sim.getClocktime()}")
+        self.myGUI.println("\nDistancetable:")
+        header = f"    dst |" + "  ".join(f"{i}" for i in range(self.sim.NUM_NODES))
+        self.myGUI.println(header)
+        self.myGUI.println("-" * len(header))
+
+        for nbr in [self.myID] + self.neighbors:
+            row = f"nbr {nbr} | " + "  ".join(F.format(self.distanceTable[nbr][dst], 3) for dst in range(self.sim.NUM_NODES))
+            self.myGUI.println(row)
+
+        self.myGUI.println("\nOur distance vector and routes:")
+        self.myGUI.println("dst  |  " + "  ".join(f"{i}" for i in range(self.sim.NUM_NODES)))
+        self.myGUI.println("-" * len(header))
+
+        costs_row = "cost  |  " + "  ".join(str(self.distanceVector[i]) for i in range(self.sim.NUM_NODES))
+        routes_row = "route |  " + "  ".join(str(self.nextHops[i]) if self.nextHops[i] is not None else "-" for i in range(self.sim.NUM_NODES))
+
+        self.myGUI.println(costs_row)
+        self.myGUI.println(routes_row)
